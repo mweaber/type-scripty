4ccab3f7714146852a7b3680d1dde20fd7b8808e
Matthew Weaber <matthewweaber89@gmail.com>
2019-11-26 19:31:19 -0500

    Basic starting point

--- using-ts.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1"); // Adding the ! tells typescript that this will always be getting an element. It will never yield null.
-var input2 = document.getElementById("num2"); // I can add the extra to tell typescript that it will for sure be an input element.
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
