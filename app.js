9d7cd48b10f1f5c57938ab86cd850ae9a6e33d66
Matthew Weaber <matthewweaber89@gmail.com>
2019-11-27 19:28:39 -0500

    TS objects and arrays aint so tough

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ var person = {
     age: 30
 };
 console.log(person.name);
+var bigObject = {
+    item: 'hockey stick',
+    grip: 'non-stick',
+    manufacturers: ['warrior', 'reebok', 'bauer'],
+    curves: {
+        open: 'toe',
+        closed: 'heel',
+        flex: 90,
+        taped: ['white', 'black', 100]
+    }
+};
+console.log(bigObject.curves.flex);
+for (var _i = 0, _a = bigObject.manufacturers; _i < _a.length; _i++) {
+    var thingy = _a[_i];
+    console.log(thingy);
+}
--- app.ts
+++ app.ts
@@ -7,3 +7,30 @@ const person: {
 };
 
 console.log(person.name);
+
+const bigObject: {
+  item: string;
+  grip: string;
+  manufacturers: string[];
+  curves: {
+    open: string;
+    closed: string;
+    flex: number;
+    taped: any[];
+  };
+} = {
+  item: "hockey stick",
+  grip: "non-stick",
+  manufacturers: ["warrior", "reebok", "bauer"],
+  curves: {
+    open: "toe",
+    closed: "heel",
+    flex: 90,
+    taped: ["white", "black", 100]
+  }
+};
+console.log(bigObject.curves.flex);
+
+for (const thingy of bigObject.manufacturers) {
+  console.log(thingy);
+}
